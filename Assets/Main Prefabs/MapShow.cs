14477914c6f77147332e4d3b70f946ceb0babcd7
Evan Darling <112196089+Darling-Evan@users.noreply.github.com>
2023-10-02 20:52:51 -0500

    ScuffedMapButton
    Press M to reveal the map while playing the game. Mad scuffed rn but it works great. I put it on the Player prefab so it is on the MainMap.

---
+++ Assets/Main Prefabs/MapShow.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapShow : MonoBehaviour
+{
+    public static bool mapIsShowing = false;
+
+    public GameObject mapShowingUI;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.M))
+        {
+            if(mapIsShowing)
+            {
+                RevealMap();
+            }
+            else
+            {
+                HideMap();
+            }
+        }
+    }
+
+    void RevealMap()
+    {
+        mapShowingUI.SetActive(false);
+        mapIsShowing = false;
+    }
+
+    void HideMap()
+    {
+        mapShowingUI.SetActive(true);
+        mapIsShowing = true;
+    }
+}
